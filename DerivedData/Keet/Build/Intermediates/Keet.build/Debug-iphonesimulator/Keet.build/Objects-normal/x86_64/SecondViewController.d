4fdf23a18cc8f362b26fb281a96212999c44daf6
Eduardo Cristerna <ecristerna@icluod.com>
2015-04-13 19:18:33 -0500

    LaunchScreen

---
+++ DerivedData/Keet/Build/Intermediates/Keet.build/Debug-iphonesimulator/Keet.build/Objects-normal/x86_64/SecondViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/eduardocristerna/Documents/6S/Móviles/Proyecto/Keet/Keet/SecondViewController.m \
+  /Users/eduardocristerna/Documents/6S/Móviles/Proyecto/Keet/Keet/SecondViewController.h
